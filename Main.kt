482f94c69f5cf1bc28ef8d294a08b8eaa049b1a8
Alexander Boev <97745889+xfoxcom@users.noreply.github.com>
2022-07-31 13:22:11 +0300

    changed stringbuilder to mutableLists

--- Main.kt
+++ Main.kt
@@ -1,23 +1,57 @@
 package minesweeper
+
 import kotlin.random.Random
 
 fun main() {
     print("How many mines do you want on the field? ")
     val mines = readln().toInt()
-    val listOfPosition = mutableListOf<Int>()
+    val listOfPosition = mutableListOf<String>()
+    val list = mutableListOf(
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", ".")
+    )
 
     while (listOfPosition.distinct().size != mines) {
-        listOfPosition.add(Random.nextInt(0, 81))
+        listOfPosition.add("" + Random.nextInt(0, 8) + Random.nextInt(0, 8))
     }
-    val fieldEmpty = StringBuilder(".................................................................................")
-    for (c in fieldEmpty.indices) {
-        for (i in listOfPosition) {
-            if (c == i) fieldEmpty[c] = 'X'
+
+    for (i in listOfPosition) {
+        if (i.toInt() in 0..8) list[0][i.toInt()] = "X" else list[i.toInt()/10][i.toInt()%10] = "X"
+    }
+
+    scan(list)
+
+    for (strings in list) {
+        for (string in strings) {
+            print(string)
         }
+        println()
     }
-    for (e in 9..81 step 10) {
-        fieldEmpty.insert(e, "\n")
-    }
-    print(fieldEmpty)
+}
+
+fun scan (line: MutableList<MutableList<String>>) {
+   for (i in 0..8) {
+       for (j in 0..8) {
+           var count = 0
+           if (line[i][j] != "X") {
+               try { if (line[i-1][j-1] == "X") count++  } catch (e: IndexOutOfBoundsException) { e.message }
+               try { if (line[i-1][j] == "X") count++ } catch (e: IndexOutOfBoundsException) { e.message }
+               try { if (line[i-1][j+1] == "X") count++ } catch (e: IndexOutOfBoundsException) { e.message }
+               try { if (line[i][j+1] == "X") count++ } catch (e: IndexOutOfBoundsException) { e.message }
+               try { if (line[i][j-1] == "X") count++ } catch (e: IndexOutOfBoundsException) { e.message }
+               try { if (line[i+1][j-1] == "X") count++ } catch (e: IndexOutOfBoundsException) { e.message }
+               try { if (line[i+1][j] == "X") count++ } catch (e: IndexOutOfBoundsException) { e.message }
+               try { if (line[i+1][j+1] == "X") count++ } catch (e: IndexOutOfBoundsException) { e.message }
+               if (count > 0) line[i][j] = "$count"
+           }
+       }
+   }
 }
 
